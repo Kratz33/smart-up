c66b9187ed79dca33e2ef1554a48f74d03c76ff2
Xavier Defer <diidadidadou@hotmail.fr>
2016-11-17 09:50:34 +0100

    plus de footer + background image que sur accueil

--- app/view/front/footer.php
+++ app/view/front/footer.php
@@ -1,9 +1,9 @@
-        <div class="footer">
+        <!--<div class="footer">
             <section>
                 <nav class="light-blue lighten-1" role="navigation">
                         Ceci est le footer
                 </nav>
             </section>
-        </div>
+        </div>-->
         </body>
 </html>
